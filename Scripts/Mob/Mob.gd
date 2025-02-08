f1c2ea22364080df6752383a14977864283c64eb
Sceadu <44327463+sceadu37@users.noreply.github.com>
2025-02-07 20:42:11 -0500

    Added aggro distance

--- Scripts/Mob/Mob.gd
+++ Scripts/Mob/Mob.gd
@@ -1,30 +1,32 @@
 extends CharacterBody2D
 
-var awareness = 0
-var aggression = 0
-var fear = 0
-var health = 1
+@export var awareness: float = 0.6
+@export var aggression = 0
+@export var fear = 0
+@export var health = 1
+
+var max_detection = 500
+var detection_range = max_detection * awareness
 
 @onready var player = $"../Player/CharacterBody2D"
 @onready var nav = $NavigationAgent2D
 
-var speed = 60
-var accel = 5
+@export var speed = 40
+@export var accel = 5
 
 func _physics_process(delta: float) -> void:
 	
 	var direction = Vector3()
 	
 	nav.target_position = player.global_position
-	print(nav.target_position)
-	#nav.target_position = get_global_mouse_position()
-	direction = nav.get_next_path_position() - global_position
-	direction = direction.normalized()
+	print("Distance to player: ", nav.distance_to_target())
 	
-	rotation = direction.angle()
-	
-	
-	velocity = velocity.lerp(direction * speed, accel * delta)
+	if nav.distance_to_target() <= detection_range:
+		direction = nav.get_next_path_position() - global_position
+		direction = direction.normalized()
+		
+		velocity = velocity.lerp(direction * speed, accel * delta)
+		rotation = velocity.angle()
 	
 	if velocity.length() != 0:
 		$AnimatedSprite2D.play("swim")
